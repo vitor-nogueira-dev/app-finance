8abe31323850ca64507eb64b60c4e48fd6b634af
Vitor Nogueira <vslnogueira96@gmail.com>
2023-05-01 14:47:50 -0300

    feat: implementando Touchable /Movements

--- src/components/Movements/index.js
+++ src/components/Movements/index.js
@@ -6,6 +6,18 @@ export default function Movements({ data }) {
 
   return (
     <TouchableOpacity style={styles.container} onPress={() => setShowValue(!showValue)}>
+      <Text style={styles.date}>{data.date}</Text>
+
+      <View style={styles.content}>
+        <Text style={styles.label}>{data.label}</Text>
+        {showValue ? (
+          <Text style={data.type === 1 ? styles.value : styles.expenses}>
+            {data.type === 1 ? `+R$ ${data.value}` : `-R$ ${data.value}`}
+          </Text>
+        ) : (
+          <View style={styles.skeleton}></View>
+        )}
+      </View>
     </TouchableOpacity>
   );
 }
