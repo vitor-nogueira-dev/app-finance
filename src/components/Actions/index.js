dac2f92f49603e2817a22711a2acdb7e196fa7dd
Vitor Nogueira <vslnogueira96@gmail.com>
2023-05-01 15:01:01 -0300

    feat: estilo labelButton /Actions

--- src/components/Actions/index.js
+++ src/components/Actions/index.js
@@ -1,27 +1,35 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native';
 
 import { AntDesign } from '@expo/vector-icons';
 
 export default function Actions() {
- return (
-   <ScrollView style={styles.container} horizontal={true} showsHorizontalScrollIndicator={false}>
+  return (
+    <ScrollView
+      style={styles.container}
+      horizontal={true}
+      showsHorizontalScrollIndicator={false}
+    >
+      <TouchableOpacity style={styles.actionButton}>
+        <View style={styles.areaButton}>
+          <AntDesign name="addfolder" size={26} color="#000" />
+        </View>
+        <Text style={styles.labelButton}>Entradas</Text>
+      </TouchableOpacity>
 
-    <TouchableOpacity style={styles.actionButton}>
-      <View style={styles.areaButton}>
-        <AntDesign name="addfolder" size={26} color="#000" />
-      </View>
-      <Text style={styles.labelButton}>Entradas</Text>
-    </TouchableOpacity>
-
-    <TouchableOpacity style={styles.actionButton}>
-      <View style={styles.areaButton}>
-        <AntDesign name="tagso" size={26} color="#000" />
-      </View>
-      <Text style={styles.labelButton}>Entradas</Text>
-    </TouchableOpacity>
-
-   </ScrollView>
+      <TouchableOpacity style={styles.actionButton}>
+        <View style={styles.areaButton}>
+          <AntDesign name="tagso" size={26} color="#000" />
+        </View>
+        <Text style={styles.labelButton}>Entradas</Text>
+      </TouchableOpacity>
+    </ScrollView>
   );
 }
 
@@ -45,4 +53,10 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-})
+  labelButton: {
+    marginTop: 4,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 13,
+  },
+});
