c885fa1e67592f91918d608480b7ee36977b4e43
Vitor Nogueira <vslnogueira96@gmail.com>
2023-05-01 12:42:02 -0300

    feat: importndo component Balance

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from '../../components/Header';
+import Balance from '../../components/Balance';
 
 export default function Home() {
   return (
     <View style={styles.container}>
       <Header name="Vitor Nogueira" />
-      <Text>Página inicial</Text>
+      <Balance />
     </View>
   );
 }
