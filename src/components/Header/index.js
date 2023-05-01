77782be4e8ea9a496031ea2152cf1468c5a62a2a
Vitor Nogueira <vslnogueira96@gmail.com>
2023-05-01 12:31:35 -0300

    feat: criando Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import {
+  View,
+  StyleSheet,
+  Text,
+  StatusBar,
+  TouchableOpacity,
+} from 'react-native';
+
+import { Feather } from '@expo/vector-icons';
+
+const statusBarHeight = StatusBar.currentHeight
+  ? StatusBar.currentHeight + 22
+  : 64;
+
+export default function Header({name}) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.content}>
+        <Text style={styles.userName}>{name}</Text>
+        <TouchableOpacity activeOpacity={0.9} style={styles.buttonUser}>
+          <Feather name="user" size={27} color="#fff" />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
