acc1a6af64ca397a64ce5c9593d94706b218c21f
Vitor Nogueira <vslnogueira96@gmail.com>
2023-05-01 15:08:24 -0300

    fix: separando list e buttons em utils

---
+++ src/utils/data.js
@@ -0,0 +1,37 @@
+const list = [
+  { id: 1, label: 'Aluguel', value: '1500,00', date: '01/09/22', type: 0 },
+  { id: 2, label: 'Salário', value: '7.000,00', date: '05/09/22', type: 1 },
+  { id: 3, label: 'Supermercado', value: '350,00', date: '07/09/22', type: 0 },
+  { id: 4, label: 'Água', value: '115,00', date: '28/09/22', type: 0 },
+  { id: 5, label: 'Pix', value: '850,00', date: '05/10/22', type: 0 },
+  { id: 6, label: 'Combustível', value: '180,00', date: '10/10/22', type: 0 },
+  { id: 7, label: 'Internet', value: '119,90', date: '15/10/22', type: 0 },
+  {
+    id: 8,
+    label: 'Plano de saúde',
+    value: '450,00',
+    date: '20/10/22',
+    type: 1,
+  },
+  { id: 9, label: 'Lazer', value: '250,00', date: '22/10/22', type: 0 },
+  {
+    id: 10,
+    label: 'Energia elétrica',
+    value: '220,00',
+    date: '30/10/22',
+    type: 0,
+  },
+];
+
+const buttons = [
+  { id: 1, label: 'Entradas', icon: 'addfolder', color: '#000' },
+  { id: 2, label: 'Saídas', icon: 'minuscircleo', color: '#FF0000' },
+  { id: 3, label: 'Transferências', icon: 'swap', color: '#007ACC' },
+  { id: 4, label: 'Empréstimos', icon: 'creditcard', color: '#FFA500' },
+  { id: 5, label: 'Investimentos', icon: 'linechart', color: '#008000' },
+  { id: 6, label: 'Receitas', icon: 'wallet', color: '#00BFFF' },
+  { id: 7, label: 'Despesas', icon: 'shoppingcart', color: '#FF4500' },
+  { id: 8, label: 'Economias', icon: 'save', color: '#32CD32' },
+];
+
+export { list };
