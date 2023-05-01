3c3d06c4961472e70f89598e28c36f80dcd804d1
Vitor Nogueira <vslnogueira96@gmail.com>
2023-05-01 14:21:32 -0300

    feat: criando componente Movements

---
+++ src/components/Movements/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function Movements() {
+  return (
+    <View style={styles.container}>
+      <Text>Movements</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+
+  }
+});
