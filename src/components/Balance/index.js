2e6425d7c0c71f53c9aff1b26804fa522f2900a1
Vitor Nogueira <vslnogueira96@gmail.com>
2023-05-01 12:39:50 -0300

    feat: criando View saldo

--- src/components/Balance/index.js
+++ src/components/Balance/index.js
@@ -4,14 +4,20 @@ import { View, Text, StyleSheet } from 'react-native';
 export default function Balance() {
   return (
     <View style={styles.container}>
-      <Text>Balance</Text>
+
+      <View style={styles.item}>
+        <Text style={styles.itemTitle}>Saldo</Text>
+        <View style={styles.content}>
+          <Text style={styles.currencySymbol}>R$</Text>
+          <Text style={styles.balance}>15.000,00</Text>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#fff',
   },
 });
