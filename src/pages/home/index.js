0dffe7380a2a8ec878ff0c6d0b73dabb06f84a41
Vitor Nogueira <vslnogueira96@gmail.com>
2023-05-01 16:49:39 -0300

    fix: removendo import array buttons

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,30 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
+
 import Header from '../../components/Header';
 import Balance from '../../components/Balance';
 import Movements from '../../components/Movements';
 import Actions from '../../components/Actions';
-import {list} from '../../utils/data';
 
+import list from '../../utils/data';
 
 export default function Home() {
   return (
     <View style={styles.container}>
-
       <Header name="Vitor Nogueira" />
 
       <Balance saldo="+R$5690,00" expenses="-R$3478,00" />
-      
+
       <Actions />
 
       <Text style={styles.title}>Últimas movimentações</Text>
 
-      <FlatList style={styles.list}
+      <FlatList
+        style={styles.list}
         data={list}
         keyExtractor={(item) => String(item.id)}
         showsVerticalScrollIndicator={false}
-        renderItem={({ item }) => <Movements data={item}/>}
-          
+        renderItem={({ item }) => <Movements data={item} />}
       />
     </View>
   );
