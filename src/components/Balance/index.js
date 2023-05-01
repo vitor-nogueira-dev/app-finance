e7c11b22131ab4a0b7c8bbfe3164761888b05f7c
Vitor Nogueira <vslnogueira96@gmail.com>
2023-05-01 12:49:45 -0300

    feat: criando estilo itemTitle/Balance

--- src/components/Balance/index.js
+++ src/components/Balance/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-export default function Balance() {
+export default function Balance({saldo, expenses}) {
   return (
     <View style={styles.container}>
-
       <View style={styles.item}>
         <Text style={styles.itemTitle}>Saldo</Text>
         <View style={styles.content}>
           <Text style={styles.currencySymbol}>R$</Text>
-          <Text style={styles.balance}>15.000,00</Text>
+          <Text style={styles.balance}>{saldo}</Text>
         </View>
       </View>
 
@@ -17,10 +16,9 @@ export default function Balance() {
         <Text style={styles.itemTitle}>Gastos</Text>
         <View style={styles.content}>
           <Text style={styles.currencySymbol}>R$</Text>
-          <Text style={styles.balance}>7.000,00</Text>
+          <Text style={styles.expenses}>{expenses}</Text>
         </View>
       </View>
-
     </View>
   );
 }
@@ -39,5 +37,9 @@ const styles = StyleSheet.create({
     paddingTop: 22,
     paddingBottom: 22,
     zIndex: 99,
+  },
+  itemTitle: {
+    fontSize: 20,
+    color: '#dadada',
   },
 });
