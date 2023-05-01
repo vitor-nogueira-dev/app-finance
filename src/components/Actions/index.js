3dd38fd64bcd040cc33b0ac422a8409fcc1f10e8
Vitor Nogueira <vslnogueira96@gmail.com>
2023-05-01 14:52:11 -0300

    feat: criando componente /Actions

---
+++ src/components/Actions/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function Actions() {
+ return (
+   <View style={styles.container}>
+    <Text>Teste</Text>
+   </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {}
+})
