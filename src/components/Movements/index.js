4a5f0f803a7f722becb9d6ab06f767ebd53aab92
Vitor Nogueira <vslnogueira96@gmail.com>
2023-05-01 16:44:54 -0300

    feat: adicionando animações View e Text Movements

--- src/components/Movements/index.js
+++ src/components/Movements/index.js
@@ -1,21 +1,53 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
+import { MotiView, AnimatePresence, MotiText } from 'moti';
+
 export default function Movements({ data }) {
   const [showValue, setShowValue] = useState(false);
 
   return (
-    <TouchableOpacity style={styles.container} onPress={() => setShowValue(!showValue)}>
+    <TouchableOpacity
+      style={styles.container}
+      onPress={() => setShowValue(!showValue)}
+    >
       <Text style={styles.date}>{data.date}</Text>
 
       <View style={styles.content}>
         <Text style={styles.label}>{data.label}</Text>
         {showValue ? (
-          <Text style={data.type === 1 ? styles.value : styles.expenses}>
-            {data.type === 1 ? `+R$ ${data.value}` : `-R$ ${data.value}`}
-          </Text>
+          <AnimatePresence exitBeforeEnter>
+            <MotiText
+              from={{
+                translateX: 100,
+              }}
+              animate={{
+                translateX: 0,
+              }}
+              transition={{
+                type: 'spring',
+                duration: 500,
+              }}
+              style={data.type === 1 ? styles.value : styles.expenses}
+            >
+              {data.type === 1 ? `+R$ ${data.value}` : `-R$ ${data.value}`}
+            </MotiText>
+          </AnimatePresence>
         ) : (
-          <View style={styles.skeleton}></View>
+          <AnimatePresence exitBeforeEnter>
+            <MotiView
+              from={{
+                opacity: 0,
+              }}
+              animate={{
+                opacity: 1,
+              }}
+              transition={{
+                type: 'spring',
+              }}
+              style={styles.skeleton}
+            ></MotiView>
+          </AnimatePresence>
         )}
       </View>
     </TouchableOpacity>
@@ -59,5 +91,5 @@ const styles = StyleSheet.create({
     height: 10,
     backgroundColor: '#DADADA',
     borderRadius: 8,
-  }
+  },
 });
